41ddee18bcd13f98186efd398d3373d3567f243f
Evan Khosh <evank43@nycstudents.net>
2025-12-03 11:00:17 -0500

    Wrote very basic framework for menu and game

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,21 @@
 # SoftDev
 # P01 -- ArRESTed Development
 # TBD
+
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def menu():
+
+    if 'submit' in request.form:
+        return redirect(url_for('game'))
+    
+    return render_template('menu.html')
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+
+    return render_template('game.html')

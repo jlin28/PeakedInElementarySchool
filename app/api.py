778769fd8e4a8e1b7527a4ca26b1752f10464dbf
Sean <szheng60@stuy.edu>
2025-12-05 11:47:22 -0500

    worked on api calling

--- app/api.py
+++ app/api.py
@@ -1,5 +1,6 @@
 import urllib.request
 import json
+import random
 
 #setup
 with open("keys/key_MoviesAPI.txt", "r") as f:
@@ -14,20 +15,28 @@ with open("keys/key_SuperheroAPI.txt", "r") as f:
 with open("keys/key_thesaurus.txt", "r") as f:
     THESAURUS_KEY = f.read().strip()
 
-MOVIE_URL = f"https://www.omdbapi.com/?i=tt3896198&apikey={MOVIE_KEY}"
 SPANISH_ENGLISH_URL = f"https://dictionaryapi.com/api/v3/references/spanish/json/test?key={SPANISH_ENGLISH_KEY}"
 SUPERHERO_URL = f"https://www.superheroapi.com/api.php/{SUPERHERO_KEY}/1"
 THESAURUS_URL = f"https://dictionaryapi.com/api/v3/references/thesaurus/json/test?key={THESAURUS_KEY}"
 RICK_AND_MORTY_URL = "https://rickandmortyapi.com/api"
-COUNTRIES_URL = "https://restcountries.com/v3.1/capital/"
+COUNTRIES_URL = "https://restcountries.com/v3.1/capital/all"
 
+#gives a random set of data for specific api
 def apiCall(api):
     if api == "movie":
+        ID = getMovieID()
+        MOVIE_URL = f"https://www.omdbapi.com/?i={ID}&apikey={MOVIE_KEY}"
         url = MOVIE_URL
     if api == "spanish":
         url = SPANISH_ENGLISH_URL
-    if api = "superhero":
+    if api == "superhero":
         url = SUPERHERO_URL
+    if api == "thesaurus":
+        url = THESAURUS_URL
+    if api == "rick":
+        url = RICK_AND_MORTY_URL
+    if api == "country":
+        url = COUNTRIES_URL
 
     with urllib.request.urlopen(url) as response:
         raw_data = response.read()
@@ -35,5 +44,14 @@ def apiCall(api):
     data = json.loads(raw_data)
 
     return data
+
+def getMovieID():
+    id = ""
+    for i in range(7):
+        num = random.randint(0, 9)
+        id = id + str(num)
+    if id == "0000000":
+        id = getMovieID()
+    return "tt" + id
 
 print(apiCall("movie"))

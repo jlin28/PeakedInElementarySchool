237ec1de284d9534dcf601a35e4277618a4c10ce
Joyce Lin <joycel78@nycstudents.net>
2025-12-06 23:06:16 -0500

    continuing on menu stuff + devlog

--- app/__init__.py
+++ app/__init__.py
@@ -4,22 +4,49 @@
 # P01 -- ArRESTed Development
 # TBD
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
+from datetime import time
 from db import *
 
 app = Flask(__name__)
+app.secret_key = 'help'
 
 @app.route('/', methods=['GET', 'POST'])
 def menu():
     print(request.form) # for testing purposes
+
+    # SETS DEFAULT SETTINGS
+    difficulties = ['', '', '']
+    setting1=''
+    setting2=''
+
     if request.method == 'POST':
-        create_questions()
-        create_game_data()
+        # ADDS SETTINGS TO SESSION
+        session.clear()
+        for x,y in request.form.to_dict().items():
+            session[x] = y;
 
-        return redirect(url_for('game'))
+    #    create_questions()
+    #    create_game_data()
 
-    return render_template('menu.html')
+        # return redirect(url_for('game'))
+
+    # SETS PREVIOUS SETTINGS
+    if 'difficulty' in session:
+        difficulties[int(session['difficulty'])] = 'checked'
+    else: difficulties[0] = 'checked'
+    if 'setting1' in session:
+        setting1='checked'
+    if 'setting2' in session:
+        setting2='checked'
+
+    return render_template('menu.html',
+                            dEasy = difficulties[0],
+                            dMed = difficulties[1],
+                            dHard = difficulties[2],
+                            placeholder1=setting1,
+                            placeholder2=setting2)
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():

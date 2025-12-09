2e49938cda7ca941ee9fafb04517b114be8eee9f
szheng60stuy <szheng60@stuy.edu>
2025-12-08 22:05:23 -0500

    movie cache and error handling for api

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 from datetime import time
 from db import *
+from pprint import pprint
+from api import apiCall
+from db import add_film
 
 app = Flask(__name__)
 app.secret_key = 'help'
@@ -52,6 +55,27 @@ def menu():
 def game():
 
     return render_template('game.html')
+
+@app.route('/test', methods=['GET', 'POST'])
+def testError():
+
+    ######### FOR ERROR HANDLING TESTING PURPOSES ####################
+    try:
+        data = apiCall("film")
+        add_film(data)
+        return data
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+        return redirect('/error')
+
+    ##################################################################
+
+    return render_template('game.html')
+
+@app.route('/error')
+def error_page():
+    #just to initialize the error handling part (we can polish it up later)
+    return "oopsies, we had an error :C"
 
 # RUN FLASK
 if __name__=='__main__':

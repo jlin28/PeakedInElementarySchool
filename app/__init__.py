42954c5b32bbc53d4a24af7d23e4ba6e837c88e5
szheng60stuy <szheng60@stuy.edu>
2025-12-18 23:18:49 -0500

    fixed

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ def menu():
     else: curTime = 1
 
     # ALL POSSIBLE QUESTION TYPES
-    question_categories = ['movies and series', 'countries', 'spanish', 'superheroes', 'synonyms','rick and morty']
+    question_categories = ['OMDB', 'Countries', 'Spanish', 'Superheroes', 'Synonyms','RickAndMorty']
 
     # SETS DEFAULT SETTINGS
     difficulties = ['checked', '', '']

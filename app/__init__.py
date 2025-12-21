81dc707555c44641512a9ce52d0983c0b9901685
szheng60stuy <szheng60@stuy.edu>
2025-12-21 15:37:45 -0500

    e

--- app/__init__.py
+++ app/__init__.py
@@ -72,7 +72,7 @@ def menu():
             if cat in data:
                 selected_categories.append(cat)
         session['categories'] = selected_categories.copy()
-        print(session['categories'])
+        
         if 'singleplayer' in data:
             reset_board()
             create_game_data()
